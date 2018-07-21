4ce3a4960d1dc9ea943cf4e509bbd11ec7278bee
Wei Jen Wang <weijen.wang@jkos.com>
2018-07-21 15:31:07 +0800

    feat(Mainn): connect all elements

--- TDD/ViewController.swift
+++ TDD/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var inputFIeld: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func calculate() {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
